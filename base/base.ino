bd1ae3888e2c3722ed818a2d4ec8dadab07da37d
Václav Pavlíček <vendasky@email.cz>
2019-04-18 11:05:23 +0200

    Reduce screens data only to three screens.

--- base/base.ino
+++ base/base.ino
@@ -6,9 +6,9 @@
 #define UCG_INTERRUPT_SAFE
 #endif
 
-uint8_t screenNum = 3;
+uint8_t screenNum = 2;
 
-#define SCREENS_COUNT 6
+#define SCREENS_COUNT 3
 
 #define Serial SerialUSB
 
@@ -20,9 +20,23 @@ uint8_t screenNum = 3;
 #define BUTTON_2 4
 #define BUTTON_3 3
 
+// RFM69
+#define NETWORKID       0   // Must be the same for all nodes (0 to 255)
+#define MYNODEID        2   // My node ID (0 to 255)
+#define FREQUENCY       RF69_433MHZ
+#define FREQUENCYSPECIFIC 433102000  // Should be value in Hz, now 433 Mhz will be set
+#define chip_select_pin   43
+#define interupt_pin    9
+
+bool isRadioOk = true;
+
+RFM69 radio(chip_select_pin, interupt_pin, true);
+
 Ucglib_ST7735_18x128x160_HWSPI ucg(6, 7, -1);
 OcsGraphics ocsDesign(ucg);
+OcsStorage ocsData(ocsDesign);
 
+OcsStorage::message income;
 
 void setup()
 {
@@ -40,6 +54,17 @@ void setup()
   pinMode(BUTTON_1, INPUT);
   pinMode(BUTTON_2, INPUT);
   pinMode(BUTTON_3, INPUT);
+  
+  if(!radio.initialize(FREQUENCY, MYNODEID, NETWORKID))
+  {
+    isRadioOk = false;
+    Serial.println("RFM69HW initialization failed!");
+  }
+  else
+  {
+    radio.setFrequency(FREQUENCYSPECIFIC);
+    radio.setHighPower(true); // Always use this for RFM69HW
+  }
 }
 
 void loop()
@@ -53,18 +78,54 @@ void loop()
     if (screenNum != 1) {
       screenNum--;
       ocsDesign.drawScreen(screenNum);
+      delay(300);
     }
-    delay(100);
   } else if (button2 == LOW) {
-    screenNum = 3;
+    screenNum = 2;
     ocsDesign.drawScreen(screenNum);
-    delay(100);
+    delay(300);
   } else if (button3 == LOW) {
     if (screenNum != SCREENS_COUNT) {
       screenNum++;
       ocsDesign.drawScreen(screenNum);
-      delay(100);
+      delay(300);
     }
   }
-  Serial.println("Screen: " + String(screenNum));
+
+
+  if (radio.receiveDone()) // Got one!
+  {
+    /*Serial.println("messageId: " + String(income.messageId));
+    Serial.println("temperatureCanSat: " + String(income.temperatureCanSat));
+    Serial.println("temperatureMPU: " + String(income.temperatureMPU));
+    Serial.println("temperatureExternal: " + String(income.temperatureExternal));
+    Serial.println("pressureCanSat: " + String(income.pressureCanSat));
+    Serial.println("pressureExternal: " + String(income.pressureExternal));
+    Serial.println("humidityCanSat: " + String(income.humidityCanSat));
+    Serial.println("humidityExternal: " + String(income.humidityExternal));
+    Serial.println("altitudeCanSat: " + String(income.altitudeCanSat));
+    Serial.println("altitudeExternal: " + String(income.altitudeExternal));
+    Serial.println("uvIndex: " + String(income.uvIndex));
+    Serial.println("lightIntensity: " + String(income.lightIntensity));
+    Serial.println("accelerationX: " + String(income.accelerationX));
+    Serial.println("accelerationY: " + String(income.accelerationY));
+    Serial.println("accelerationZ: " + String(income.accelerationZ));
+    Serial.println("rotationX: " + String(income.rotationX));
+    Serial.println("rotationY: " + String(income.rotationY));
+    Serial.println("rotationZ: " + String(income.rotationZ));
+    Serial.println("year: " + String(income.year));
+    Serial.println("month: " + String(income.month));
+    Serial.println("day: " + String(income.day));
+    Serial.println("hour: " + String(income.hour));
+    Serial.println("minute: " + String(income.minute));
+    Serial.println("second: " + String(income.second));
+    Serial.println("numberOfSatellites: " + String(income.numberOfSatellites));
+    Serial.println("latInt: " + String(income.latInt));
+    Serial.println("lonInt: " + String(income.lonInt));
+    Serial.println("latAfterDot: " + String(income.latAfterDot));
+    Serial.println("lonAfterDot: " + String(income.lonAfterDot));*/
+    income = *(OcsStorage::message*)radio.DATA;
+    ocsData.Update(income, screenNum);
+    delay(300);
+  }
 }
